a63212eb2b21c05a741c77c672fbb2743594fb43
Md Ataullah Masood <info.mdataullah@gmail.com>
2026-02-15 14:08:26 +0530

    jsx understanding

--- react-vite/src/main.jsx
+++ react-vite/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>Visit Google</a>
+
 )
+
+const anotherUser = "BISWA"
+const reactElement = React.createElement(
+  'a',
+  {href: "https://google.com", target: "_blank"},
+  'click me to visit google ',
+  anotherUser
+)
+
+createRoot(document.getElementById('root')).render(
+    <App />    //? jab function ko krna hoga usko < iske ander dalna hoga/>
+    //reactElement  //? jab object ko render krna hoga direct krengein
+)
+
+
+
