46e60419887884e7e86cb0cd824ac0423d2c8d06
Md Ataullah Masood <info.mdataullah@gmail.com>
2026-02-22 13:58:39 +0530

    making backend using appwrite

---
+++ appWrite-Backend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+  
+
+  return (
+    <>
+      <h2>Blog App AppWrite</h2>
+    </>
+  )
+}
+
+export default App
+
+
+
+//? environment variables
